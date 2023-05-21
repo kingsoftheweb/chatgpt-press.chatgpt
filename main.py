ead52ec56e05827bdbf74bb1df03b25e6e974344
Kings Of The Web <kingsofthewebofficial@gmail.com>
2023-05-21 16:46:40 -0400

    Refactor "Posts" endpoints to be called from the `Posts` class

--- main.py
+++ main.py
@@ -21,38 +21,44 @@ app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.c
 
 
 def genAuth(site=False):
-    if(not site):return noSiteException()
+    if (not site): return noSiteException()
     uid = str(uuid.uuid1())
     try:
         r = requests.get(site)
-        if(r.status_code != 200):  return {"error":"Please check your web address. It seems invalid."}
+        if (r.status_code != 200):  return {"error": "Please check your web address. It seems invalid."}
     except:
-        return {"error":"Please check your web address. It seems invalid."}
-    if(requests.get(site+"/authenticate-chatgptpress").status_code==200):
-        login_url = site+"/authenticate-chatgptpress?redirect_to=http://localhost:5003/login/"+uid
-        _CONF[uid]={"status":True,"site":site}
-        sid = jwt.encode({"uid":uid,"exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=85)}, _SECRET, algorithm="HS256")
+        return {"error": "Please check your web address. It seems invalid."}
+    if (requests.get(site + "/authenticate-chatgptpress").status_code == 200):
+        login_url = site + "/authenticate-chatgptpress?redirect_to=http://localhost:5003/login/" + uid
+        _CONF[uid] = {"status": True, "site": site}
+        sid = jwt.encode({"uid": uid, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=85)}, _SECRET,
+                         algorithm="HS256")
         return {
-            "uid":sid,
-            "action":"You need to login manually. chatGPT will show login link: "+login_url
-            }
+            "uid": sid,
+            "action": "You need to login manually. chatGPT will show login link: " + login_url
+        }
     else:
-        return{"error":"chatgptpress plugin not found in your website. please install the plugin and come back again."}
+        return {
+            "error": "chatgptpress plugin not found in your website. please install the plugin and come back again."}
+
 
 def indigest(token):
     try:
         return jwt.decode(token, _SECRET, verify_exp=True, algorithms=["HS256"])
     except:
-        return {"error":"The request session has been expired. please give your wp site address to start again."}
+        return {"error": "The request session has been expired. please give your wp site address to start again."}
+
 
 def noSiteException():
     return {"error": "provide you wp site address to continue."}
 
+
 def validSite(site):
-    site = site.replace("https://","")
-    site = site.replace("http://","")
-    site = "https://"+site
+    site = site.replace("https://", "")
+    site = site.replace("http://", "")
+    site = "https://" + site
     return site
+
 
 def logged():
     def wrapper(func):
@@ -60,18 +66,22 @@ def logged():
         async def wrapped(*args, **kwargs):
             token = kwargs["token"]
             try:
-                ind=indigest(token)
-                uid= ind["uid"]
-                usr=ind["user"]
+                ind = indigest(token)
+                uid = ind["uid"]
+                usr = ind["user"]
                 password = ind["pass"]
-                if(uid and usr and password):
+                if (uid and usr and password):
                     print("ok")
                 else:
-                   return {"error":"The request session has been expired. please give your wp site address to start again."}
+                    return {
+                        "error": "The request session has been expired. please give your wp site address to start again."}
             except:
-                return {"error":"The request session has been expired. please give your wp site address to start again."}
+                return {
+                    "error": "The request session has been expired. please give your wp site address to start again."}
             return await func(*args, **kwargs)
+
         return wrapped
+
     return wrapper
 
 
@@ -81,38 +91,40 @@ async def get_token_from_chat():
     try:
         uid = indigest(request.args.get('uid'))["uid"]
     except:
-        return  {"error":"login link expired","action":"Get a new login link from /login"}
+        return {"error": "login link expired", "action": "Get a new login link from /login"}
     inConf = _CONF.get(uid)
-    if(not inConf): return  {"error":"login link expired","action":"Get a new login link from /login"}
+    if (not inConf): return {"error": "login link expired", "action": "Get a new login link from /login"}
     status = inConf.get("status")
     token = inConf.get("token")
-    if(status and not token): return genAuth()
-    if(status and token):return {"token":inConf["token"]}
-    if(not status): return {"error":"Not registed yet"}
+    if (status and not token): return genAuth()
+    if (status and token): return {"token": inConf["token"]}
+    if (not status): return {"error": "Not registed yet"}
     return genAuth()
+
 
 @app.post("/login")
 async def login_to_chat():
     return genAuth(validSite(request.args.get("site")))
 
+
 @app.get("/login/<string:uid>")
 async def login_to_site(uid):
     url = "/wp-json/wp/v2/users/me"
-    if(not _CONF.get(uid)):return "Bad or illegal Request"
+    if (not _CONF.get(uid)): return "Bad or illegal Request"
     try:
-        auth = (request.args.get("username"),request.args.get("appPassword"))
-        r = requests.post(_CONF.get(uid)["site"]+url, auth = auth).json()
-        if(r["id"]):status="Login successfull, please go back to GPT conversation."
+        auth = (request.args.get("username"), request.args.get("appPassword"))
+        r = requests.post(_CONF.get(uid)["site"] + url, auth=auth).json()
+        if (r["id"]): status = "Login successfull, please go back to GPT conversation."
         print(r["id"])
         json_payload = {
             "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=85),
             "site": _CONF.get(uid)["site"],
-            "user": request.args.get("username"), 
+            "user": request.args.get("username"),
             "pass": request.args.get("appPassword"),
-            "uid":uid,
-            "author":str(r["id"])
+            "uid": uid,
+            "author": str(r["id"])
         }
-        _CONF[uid]["token"]=jwt.encode(json_payload, _SECRET, algorithm="HS256")
+        _CONF[uid]["token"] = jwt.encode(json_payload, _SECRET, algorithm="HS256")
     except:
         print("error login")
         return """
@@ -121,7 +133,7 @@ async def login_to_site(uid):
                 window.close();
             </script>
         """
-    if(_CONF.get(uid)):
+    if (_CONF.get(uid)):
         return """
             <script>
                 alert("Login successfull. Please go back to chatGPT and continue your conversation.");
@@ -136,53 +148,40 @@ async def login_to_site(uid):
             </script>
         """
 
+
+########################
+# POSTS ################
+########################
 @app.get("/posts/<string:token>")
 async def get_posts(token):
-    token = indigest(token)
-    if(token.get("error")):return token
-    url = token["site"]+"/wp-json/wp/v2/posts?"
-    if(request.args.get('postId')):
-        url = token["site"]+"/wp-json/wp/v2/posts/"+request.args.get('postId')
-        print(url)
-        r = requests.get(url)
-        return quart.Response(response=json.dumps(r.json()), status=200)
-    if(request.args.get('postType')):url = token["site"]+'/wp-json/wp/v2/'+request.args.get('postType')+"?"  
-    url+="_fields=id,date,link&per_page=10"
-    after = request.args.get('afterDate')
-    if(after):url += "&after="+parse(after).isoformat()
-    before = request.args.get('beforeDate')
-    if(before):url += "&before="+parse(before).isoformat()
-    r = requests.get(url)
-    return quart.Response(response=json.dumps(r.json()), status=200)
+    res = await Posts().get_posts(token)
+    return quart.Response(response=res, status=200)
 
 
 @app.post("/addPost/<string:token>")
 @logged()
 async def add_new_post(token):
-    res = Posts.add_new_post(token)
+    res = await Posts().add_new(token)
     return quart.Response(response=res, status=200)
 
 
 @app.post("/updatePost/<string:token>")
 @logged()
-async def update_existed_post(token):
-    token = indigest(token)
-    url = token["site"]+"/wp-json/wp/v2/posts/"+request.args.get('post_id')
-    data = {
-        "title":request.args.get('title')
-    }
-    r = requests.post(url, data = data, auth = (token["user"],token["pass"]))
-    return quart.Response(response=json.dumps(r.json()), status=200)
+async def update_post(token):
+    res = await Posts().update(token)
+    return quart.Response(response=res, status=200)
+
 
 @app.post("/deletePost/<string:token>/<string:postId>")
 @logged()
-async def delete_post(token,postId):
-    token = indigest(token)
-    url = token["site"]+"/wp-json/wp/v2/posts/"+postId
-    r = requests.delete(url, auth = (token["user"],token["pass"]))
-    return quart.Response(response=json.dumps(r.json()), status=200)
+async def delete_post(token, postId):
+    res = await Posts().delete(token, postId)
+    return quart.Response(response=res, status=200)
 
 
+########################
+# PLUGINS ##############
+########################
 @app.post("/findPlugin/<string:token>/<string:keyword>")
 @logged()
 async def find_plugin(token, keyword):
@@ -192,10 +191,10 @@ async def find_plugin(token, keyword):
 
 @app.post("/installPlugin/<string:token>/<string:slug>")
 @logged()
-async def install_plugin(token,slug):
+async def install_plugin(token, slug):
     token = indigest(token)
-    url = token["site"]+"/wp-json/chatgptpress/v1/plugins/install"
-    r = requests.post(url, json={"slug":slug}, auth = (token["user"],token["pass"]))
+    url = token["site"] + "/wp-json/chatgptpress/v1/plugins/install"
+    r = requests.post(url, json={"slug": slug}, auth=(token["user"], token["pass"]))
     res = r.json()
     return quart.Response(response=json.dumps(res), status=200)
 
@@ -204,19 +203,19 @@ async def install_plugin(token,slug):
 @logged()
 async def find_bug(token):
     token = indigest(token)
-    url = token["site"]+"/wp-json/chatgptpress/v1/debuglog/debug"
-    r = requests.get(url, auth = (token["user"],token["pass"]))
+    url = token["site"] + "/wp-json/chatgptpress/v1/debuglog/debug"
+    r = requests.get(url, auth=(token["user"], token["pass"]))
     pList = r.json()
     resList = []
     counter = 0
     for p in pList:
-        counter+=1
+        counter += 1
         p.pop("date")
         p.pop("time")
         p.pop("timeZone")
         resList.append(p)
-        if(counter>=50): break
-    res = {"action":"chatGPT will suggest how to fix this errors", "plugins":resList}
+        if (counter >= 50): break
+    res = {"action": "chatGPT will suggest how to fix this errors", "plugins": resList}
     return quart.Response(response=json.dumps(res), status=200)
 
 
@@ -225,12 +224,14 @@ async def plugin_logo():
     filename = 'logo.png'
     return await quart.send_file(filename, mimetype='image/png')
 
+
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
     host = request.headers['Host']
     with open("./.well-known/ai-plugin.json") as f:
         text = f.read()
         return quart.Response(text, mimetype="text/json")
+
 
 @app.get("/openapi.yaml")
 async def openapi_spec():
@@ -239,8 +240,10 @@ async def openapi_spec():
         text = f.read()
         return quart.Response(text, mimetype="text/yaml")
 
+
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
+
 
 if __name__ == "__main__":
     main()
--- src/Posts.py
+++ src/Posts.py
@@ -1,8 +1,10 @@
 import json
 
+import quart
 import requests
 from quart import request
 from ast import literal_eval
+from dateutil.parser import parse
 
 from src.helpers import indigest
 
@@ -13,10 +15,33 @@ class Posts:
 
     This class is used to handle all the requests related to posts.
     Currently, supports:
+        - Listing posts 'get_posts()'
         - Adding new posts 'add_new_post()'
+        - Update an existing post 'update()'
+        - Deleting an existing post 'delete()'
     """
 
-    async def add_new_post(self, token):
+    #TODO; Add support for other post types (example 'recipe' post type)
+    async def get_posts(self, token):
+        token = indigest(token)
+        if token.get("error"): return token
+        url = token["site"] + "/wp-json/wp/v2/posts?"
+        if request.args.get('postId'):
+            url = token["site"] + "/wp-json/wp/v2/posts/" + request.args.get('postId')
+            print(url)
+            r = requests.get(url)
+            return quart.Response(response=json.dumps(r.json()), status=200)
+        if request.args.get('postType'): url = token["site"] + '/wp-json/wp/v2/' + request.args.get('postType') + "?"
+        url += "_fields=id,date,link&per_page=10"
+        after = request.args.get('afterDate')
+        if after: url += "&after=" + parse(after).isoformat()
+        before = request.args.get('beforeDate')
+        if before: url += "&before=" + parse(before).isoformat()
+        r = requests.get(url)
+
+        return json.dumps(r.json())
+
+    async def add_new(self, token):
         token = indigest(token)
         url = token["site"] + "/wp-json/wp/v2/posts"
         raw_data = (await request.body)
@@ -40,3 +65,21 @@ class Posts:
         if r.json(): res = json.dumps(r.json())
 
         return res
+
+    async def update(self, token):
+        token = indigest(token)
+        url = token["site"] + "/wp-json/wp/v2/posts/" + request.args.get('post_id')
+        data = {
+            "title": request.args.get('title')
+            # TODO: Add other fields for update!
+        }
+        r = requests.post(url, data=data, auth=(token["user"], token["pass"]))
+
+        return json.dumps(r.json())
+
+    async def delete(self, token, postId):
+        token = indigest(token)
+        url = token["site"] + "/wp-json/wp/v2/posts/" + postId
+        r = requests.delete(url, auth=(token["user"], token["pass"]))
+
+        return json.dumps(r.json())
