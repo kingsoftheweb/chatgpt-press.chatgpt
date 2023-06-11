b89607e782a3891c429bd1c7537a0f5d1ead63a3
therowf <therowf@gmail.com>
2023-06-11 20:51:54 +0600

    Implement the service level auth to all endpoints

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import quart
 import quart_cors
 from quart import request
 from functools import wraps
-
+import json
 from src.Posts import Posts
-
+from globals import _SERVICE_AUTH_KEY
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
@@ -41,18 +41,29 @@ async def legal_docs():
         text = f.read()
         return quart.Response(text, mimetype="text/html")
 
+########################
+# plugin service level auth ################
+########################
+def assert_auth_header(req):
+    if(req.headers.get("Authorization", None) == f"Bearer {_SERVICE_AUTH_KEY}"):
+        return True
+    else:
+        return False
+   
  
 ########################
 # POSTS ################
 ########################
 @app.get("/posts")
 async def get_posts():
+    if(not assert_auth_header(quart.request)): return quart.Response(response=json.dumps({"error":"Auth is invalid for the plugin"}), status=200)
     res = await Posts().get_posts()
     return quart.Response(response=res, status=200)
 
 
 @app.post("/posts")
 async def get_post_details():
+    if(not assert_auth_header(quart.request)):return quart.Response(response=json.dumps({"error":"Auth is invalid for the plugin"}), status=200)
     res = await Posts().get_post_details()
     return quart.Response(response=res, status=200)
 
