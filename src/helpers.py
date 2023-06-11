895825e25f246c3be8f64864a8f1b0d28b9ac6ac
therowf <therowf@gmail.com>
2023-06-11 19:22:01 +0600

    improve error raising

--- src/helpers.py
+++ src/helpers.py
@@ -1,11 +1,11 @@
 import requests
 
-
 def validate_site(site_url):
     # return a valid https url
     if site_url.startswith("http://"):
         site_url = site_url.replace("http://", "https://")
-    elif not site_url.startswith("https://"):
+    
+    if not site_url.startswith("https://"):
         site_url = "https://" + site_url
     
     try:
@@ -18,7 +18,7 @@ def validate_site(site_url):
         return {
             "error": "Please check your Web Address. It seems invalid."
         }
-    return site_url
+    return {"site":site_url,"error":False}
 
 
 def valid_post_type(postType):
